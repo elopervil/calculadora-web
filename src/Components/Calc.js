ae37401eef1f1522b8b2d96bba9ad2a605122133
elopervil <elopervil@gmail.com>
2022-08-09 21:49:56 +0200

    implement decimal number

--- src/Components/Calc.js
+++ src/Components/Calc.js
@@ -7,6 +7,7 @@ const Calc = (props) => {
     const OPERATION = 1;
     const INPUT_SECOND_NUMBER = 2;
     const RESULT = 3;
+    const DECIMAL = 4;
 
     const [value, setValue] = useState(0);
     const [firstOperator, setFirstOperator] = useState (0);
@@ -17,21 +18,36 @@ const Calc = (props) => {
     const onInput = (val) => {
         
         if (typeof val === 'number'){
+            
             if (status === INPUT_FIRST_NUMBER || status === INPUT_SECOND_NUMBER) {
                 setValue((value * 10) + val);
                 setSaveOperator(val);
             } else if (status === OPERATION) {
                 setValue(val);
                 setStatus(INPUT_SECOND_NUMBER);
+            } else if (status === DECIMAL){
+                if (value.toString().indexOf('.') !== -1){
+                    let num = `${value}${val}`;
+                    setValue(num);
+                } else {
+                    if (val !== 0){
+                        setValue(value + (val/10));  
+                    } else {
+                        let num = `${value}.${val}`;
+                        setValue(num);
+                    }
+                }
+
             }
         } else if (val === 'DEL') {
             setValue(0);
             setSaveOperator(0);
             setStatus(INPUT_FIRST_NUMBER);
         } else if (val === '.') {
+            setStatus(DECIMAL);
 
         } else if (val === '=') {
-            if (status === INPUT_SECOND_NUMBER) {
+            if (status === INPUT_SECOND_NUMBER || status === DECIMAL) {
                 switch (operation) {
                     case '+' :
                         setValue(firstOperator + value);
